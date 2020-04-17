c2a185def4e7fb2dd0b6742c6b65851b1d5819af
vinayreddyking <vinayreddyking@gmail.com>
2020-04-17 16:28:45 +0530

    Routing Done.

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  // final String id;
+  // final String title;
+  // CategoryMealsScreen(this.id, this.title);
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, String> routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final title = routeArgs['title'];
+    final id = routeArgs['id'];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(
+          'The Recipies for The Categories.',
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/category_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import './dummy_catagories.dart';
+import './category_item.dart';
+
+class CategoryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(15),
+        children: DUMMY_CATEGORIES
+            .map(
+              (dcData) => CategoryItem(
+                id: dcData.id,
+                title: dcData.title,
+                color: dcData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
